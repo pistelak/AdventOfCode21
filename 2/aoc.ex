8a5324ccb7b158abdcf7d5841d4f4989104388ec
Radek Pištělák <radek.pistelak@kiwi.com>
2021-12-02 22:19:30 +0100

    2/part two

--- 2/aoc.ex
+++ 2/aoc.ex
@@ -3,25 +3,25 @@ defmodule AdventOfCode do
   def process_file(file_name) do
     File.read!(file_name)
       |> String.split("\n")
-      |> compute(0, 0)
+      |> compute(0, 0, 0)
   end
 
-  def compute(["forward " <> rest | tail], horizontal_position, depth) do
+  def compute(["forward " <> rest | tail], horizontal_position, depth, aim) do
     value = String.to_integer(rest)
-    compute(tail, horizontal_position + value, depth)
+    compute(tail, horizontal_position + value, depth + aim * value, aim)
   end
 
-  def compute(["up " <> rest | tail], horizontal_position, depth) do
+  def compute(["up " <> rest | tail], horizontal_position, depth, aim) do
     value = String.to_integer(rest)
-    compute(tail, horizontal_position, depth - value)
+    compute(tail, horizontal_position, depth, aim - value)
   end
 
-  def compute(["down " <> rest | tail], horizontal_position, depth) do
+  def compute(["down " <> rest | tail], horizontal_position, depth, aim) do
     value = String.to_integer(rest)
-    compute(tail, horizontal_position, depth + value)
+    compute(tail, horizontal_position, depth, aim + value)
   end
 
-  def compute([], horizontal_position, depth) do
+  def compute([], horizontal_position, depth, _aim) do
     horizontal_position * depth
   end
 
