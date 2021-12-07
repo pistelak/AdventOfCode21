75639ec86547d7af6c1c2d4e46ab6fa46bb34d1a
Radek Pištělák <radek.pistelak@kiwi.com>
2021-12-07 19:29:21 +0100

    7/part two

--- 7/aoc/lib/advent_of_code.ex
+++ 7/aoc/lib/advent_of_code.ex
@@ -5,17 +5,24 @@ defmodule AdventOfCode do
     sequence = File.read!(file_name)
       |> String.split(",")
       |> Enum.map(&String.to_integer/1)
-
-    number_of_moves(sequence, median(sequence))
+      |> number_of_moves
 
   end
 
-  def number_of_moves(list, median) do
-    list |> Enum.map(&abs(&1 - median)) |> Enum.sum
+  def number_of_moves(list) do
+    min = list |> Enum.min
+    max = list |> Enum.max
+
+    min..max
+      |> Enum.map(fn destination ->
+        list |> Enum.map(&cost(&1, destination)) |> Enum.sum
+      end)
+      |> Enum.min
   end
 
-  def median(list) do
-    list |> Enum.sort |> Enum.at(div(length(list), 2))
+  def cost(initial_position, destination) do
+    n = abs(initial_position - destination)
+    n * (n + 1) / 2
   end
 
 end
