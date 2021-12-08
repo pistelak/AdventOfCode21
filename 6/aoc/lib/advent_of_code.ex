a5194caa109d81a3d4446297c458f44ffc0122cb
Radek Pištělák <radek.pistelak@kiwi.com>
2021-12-08 18:42:06 +0100

    6/part two

--- 6/aoc/lib/advent_of_code.ex
+++ 6/aoc/lib/advent_of_code.ex
@@ -5,26 +5,23 @@ defmodule AdventOfCode do
     File.read!(file_name)
       |> String.split(",", trim: true )
       |> Enum.map(&String.to_integer/1)
+      |> Enum.frequencies
+      |> then(fn frequencies -> Enum.map((0..8), fn i -> frequencies |> Map.get(i, 0) end) end)
       |> simulate(80)
-
   end
 
   @doc """
   iex> AdventOfCode.simulate([3,4,3,1,2], 18)
   26
   """
-  def simulate(fishes, 0) do
-    fishes |> Enum.count
+  def simulate(sample, number_of_steps) do
+    (1..number_of_steps)
+      |> Enum.reduce(sample, fn _, sample -> step(sample) end)
+      |> Enum.sum()
   end
 
-  def simulate(fishes, number_of_iteration_left) do
-
-    number_of_fishes_to_add = Enum.count(fishes, &(&1 == 0))
-
-    fishes = Enum.map(fishes, fn until_new_fish -> if until_new_fish == 0, do: 6, else: until_new_fish - 1 end)
-    fishes = [fishes | List.duplicate(8, number_of_fishes_to_add)] |> List.flatten
-
-    simulate(fishes, number_of_iteration_left - 1)
+  def step([a, b, c, d, e, f, g, h, i]) do
+    [b, c, d, e, f, g, h + a, i, a]
   end
 
 end
